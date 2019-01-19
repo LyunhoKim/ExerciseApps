900c60d25b65bcca9a9599fb452f7ab6dfa53788
Lyunho Kim <kimlh2@gmail.com>
2019-01-19 16:54:19 +0900

    autolayout calc

--- AutoLayout_Anchor/AutoLayout_Anchor/ViewController.swift
+++ AutoLayout_Anchor/AutoLayout_Anchor/ViewController.swift
@@ -13,6 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let myButton: UIButton = UIButton.init(type: .system)
+        myButton.setTitle("my button", for: .normal)
+        
+        self.view.addSubview(myButton)
+        
+//        self.view.translatesAutoresizingMaskIntoConstraints = false
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
+        myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 100).isActive = true
+        
+        
+        let myLabel: UILabel = UILabel.init()
+        myLabel.text = "My Label"
+        
+        self.view.addSubview(myLabel)
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        myLabel.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 50).isActive = true
+        myLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        
+        
     }
 
 

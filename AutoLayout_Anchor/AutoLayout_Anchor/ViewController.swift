d9eca12f32c1e99adeebcd2d0ab22c9f28160e86
Lyunho Kim <kimlh2@gmail.com>
2019-01-19 13:59:01 +0900

    commit apps

---
+++ AutoLayout_Anchor/AutoLayout_Anchor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout_Anchor
+//
+//  Created by Lyunho Kim on 19/01/2019.
+//  Copyright © 2019 Lyunho Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BoxOffice/BoxOffice/MovieDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MovieDetailViewController.swift
+//  BoxOffice
+//
+//  Created by Lyunho Kim on 13/01/2019.
+//  Copyright © 2019 Lyunho Kim. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        self.navigationController?.title = "영화 목록".  // ??????
+        self.navigationItem.title = "영화 목록"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

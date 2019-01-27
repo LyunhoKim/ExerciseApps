21825bc9614da5bffb1a8d67ef6412c046018622
Lyunho Kim <kimlh2@gmail.com>
2019-01-27 15:15:37 +0900

    cmoplete custom keyboard

---
+++ AutoLayout_CustomKeypad/AutoLayout_CustomKeypad/CustomKeypad.swift
@@ -0,0 +1,21 @@
+//
+//  CustomKeypad.swift
+//  AutoLayout_CustomKeypad
+//
+//  Created by Lyunho Kim on 24/01/2019.
+//  Copyright © 2019 Lyunho Kim. All rights reserved.
+//
+
+import UIKit
+
+class CustomKeypad: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

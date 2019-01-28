e2d8361a7a2c140d87f270560307d8588733e2de
Lyunho Kim <kimlh2@gmail.com>
2019-01-28 23:57:58 +0900

    graph init

---
+++ GraphView/GraphView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GraphView
+//
+//  Created by Lyunho Kim on 28/01/2019.
+//  Copyright © 2019 Lyunho Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet var graph1Height: NSLayoutConstraint!
+    @IBOutlet var graph2Height: NSLayoutConstraint!
+    @IBOutlet var graph3Height: NSLayoutConstraint!
+    @IBOutlet var graph4Height: NSLayoutConstraint!
+    @IBOutlet var graph5Height: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
